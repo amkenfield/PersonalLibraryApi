958a832b77e08eff20a32fa58fb40a764f62a8d7
Drew Kenfield <amkenfield@gmail.com>
2024-02-13 09:36:37 -0600

    Created basic CRUD API using in-memory db

---
+++ PersonalLibraryApi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalLibraryApi.Models
+{
+    public class Book
+    {
+       // title, author, publish date, pages, date acquired, rating, times read
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int TotalPages { get; set; }
+        public DateTime AcquisitionDate { get; set; }
+        public decimal Rating { get; set; }
+        public int TimesRead { get; set; }
+    }
+}
